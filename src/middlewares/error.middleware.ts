046e507863361f726037365f2c95b7356f003107
Jessicakgs <jessicakauane20@gmail.com>
2023-08-08 19:34:12 -0300

    add error middleware

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpException } from "../interfaces/HttpsException";
+
+export function errorMiddleware(
+err: HttpException,
+req: Request,
+res: Response, 
+next: NextFunction
+){
+    const status: number = err.status
+    const message: string = err.message
+
+    res.status(status).json({
+        status,
+        message,
+    });
+
+}
